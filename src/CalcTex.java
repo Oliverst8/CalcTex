1f6beac5fef04d64a016f3c2106b32122d0f9c52
Oliverst8 <Oliver@starup99.dk>
2023-11-12 15:00:48 +0100

    Made the 4 normal operations possible. It is still not possible to do fractions with numbers bigger than 1 digit, and it is not possible to do parentheses

--- src/CalcTex.java
+++ src/CalcTex.java
@@ -36,7 +36,7 @@ public class CalcTex {
         int amountFound = 0;
         for (int i = 0; i < equation.length();i++) {
             if(equation.charAt(i) == character.charAt(0)) amountFound++;
-            if(occurences/2 == amountFound) return i;
+            if(Math.ceil(occurences/2.0) == amountFound || occurences == amountFound) return i;
         }
         throw new UnsupportedOperationException("No character index found");
     }
@@ -44,36 +44,33 @@ public class CalcTex {
     public static String[] splitEquation(String equation){
         //throw new UnsupportedOperationException("Not implemented");
         String[] splitEquation = new String[3];
-        if(occuringses(equation, "+") > 0){
-            int splitI = splitingIndex(occuringses(equation, "+"), "+", equation);
-            splitEquation[0] = equation.substring(0,splitI);
-            splitEquation[1] = equation.substring(splitI+1);
-            splitEquation[2] = "+";
+        String[] operators = {"+","-","*","/"};
+        for(String operator : operators){
+            if(occuringses(equation, operator) > 0){
+                int splitI = splitingIndex(occuringses(equation, operator), operator, equation);
+                splitEquation[0] = equation.substring(0,splitI);
+                splitEquation[1] = equation.substring(splitI+1);
+                splitEquation[2] = operator;
+                return splitEquation;
+            }
         }
-
         return splitEquation;
     }
 
     public static Exp stringToExp(String equation){
-        Pattern operators = Pattern.compile("(:?[\\+\\/\\*-])");
+        if (equation == null) throw new NullPointerException("Equation cant be null");
+        Pattern operators = Pattern.compile("(:?[\\+\\/\\*\\-])");
         Matcher matcher = operators.matcher(equation);
-        if(matcher.results().count() == 1) return stringToSingleExp(equation);
+        long amountOfOperators = matcher.results().count();
+        if(amountOfOperators <= 1) return stringToSingleExp(equation);
         String[] splitEquation = splitEquation(equation);
         return combineExpressions(stringToExp(splitEquation[0]), stringToExp(splitEquation[1]), splitEquation[2]);
     }
 
     public static long occuringses(String str, String character){
-        Pattern pattern = Pattern.compile(character);
+        Pattern pattern = Pattern.compile(Pattern.quote(character));
         Matcher matcher = pattern.matcher(str);
         return matcher.results().count();
-    }
-
-    public static String mostOccuringOperator(String equation){
-        Long plus = occuringses(equation, "+");
-        //Long minus = occuringses(equation, "-");
-        //Long division = occuringses(equation, "/");
-        //Long times = occuringses(equation, "*");
-        throw new UnsupportedOperationException("Not implemented");
     }
 
     //Takes a string with one operator, and returns a matching expression
@@ -84,26 +81,50 @@ public class CalcTex {
             String[] numberExpressions = input.split("(:?\\*)");
             output = new MulExp(stringToNumExp(numberExpressions[0]), stringToNumExp(numberExpressions[1]));
         } else if(input.contains("/")){
-            throw new UnsupportedOperationException("Converting divison to exp is not implemented yet");
+            String[] numberExpressions = input.split("(:?\\/)");
+            output = new DivExp(stringToNumExp(numberExpressions[0]), stringToNumExp(numberExpressions[1]));
         } else if(input.contains("+")){
             String[] numberExpressions = input.split("(:?\\+)");
             output = new AddExp(stringToNumExp(numberExpressions[0]), stringToNumExp(numberExpressions[1]));
         } else if(input.contains("-")){
             String[] numberExpressions = input.split("-");
             output = new SubExp(stringToNumExp(numberExpressions[0]), stringToNumExp(numberExpressions[1]));
+        } else{
+            output = new NumExp(Integer.parseInt(input));
+        }
+        return output;
+    }
+
+    //Cant do fractions if they have more then on number in both spots
+    public static String replaceFrac(String equation){
+        String output = equation;
+        for(int i = 0; i < equation.length(); i++){
+            if(equation.charAt(i) == '\\'){
+                if(equation.charAt(i+1) == 'f'){
+                    output = equation.substring(0,i) + equation.charAt(i+6) + "/" + equation.charAt(i+9) + equation.substring(i+11);
+                }
+            }
         }
         return output;
     }
 
     public static String replaceAllLatex(String input){
-        return input.replace("\\cdot", "*");
+        String output = input.replace("\\cdot", "*");
+        if(output.contains("\\frac")){
+            output = replaceFrac(output);
+        }
+        return output;
     }
 
 
     public static void main(String[] args) {
         Exp equation;
         String input = input().replace(" ", "");
-        System.out.println(stringToExp(replaceAllLatex(input)).eval());
+        //System.out.println(stringToExp(replaceAllLatex(input)).eval());
+        equation = stringToExp(replaceAllLatex(input));
+        System.out.println(equation.eval());
+        //System.out.println(stringToExp("1/2+2").eval());
+        //System.out.println(replaceFrac(input));
         //equation = stringToExp(input);
         //equation.print();
         //System.out.println(input);
